a738fb9960218387505dddff86ad4e826424b1bd
owner <toltolyibh@gmail.com>
2025-03-02 18:44:34 +0900

    장바구니 사이드바 구현

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import {createContext, ReactNode, useContext, useState} from "react";
+import {ShoppingCart} from "../components/ShoppingCart.tsx";
 
 
 type ShoppingCartProviderProps = {
@@ -94,6 +95,7 @@ export function ShoppingCartProvider({children}: ShoppingCartProviderProps) {
                 cartQuantity,
             }}>
             {children}
+            <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
     )
 
