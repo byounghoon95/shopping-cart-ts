02f7f73e7ef412d8a4a980b7528422cd6269fb99
owner <toltolyibh@gmail.com>
2025-03-02 16:08:54 +0900

    라우팅 및 Navbar 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import {Container, Nav} from "react-bootstrap";
+import {Home} from "./pages/Home.tsx";
+import {Store} from "./pages/Store.tsx";
+import {About} from "./pages/About.tsx";
+import {Navbar} from "./components/Navbar.tsx";
+
 
 function App() {
   return (
-      <h1>
-          HI
-      </h1>
+      <>
+          <Navbar />
+          <Container className="mb-4">
+              <Routes>
+                  <Route path="/" element={<Home />}/>
+                  <Route path="/store" element={<Store />}/>
+                  <Route path="/about" element={<About />}/>
+              </Routes>
+          </Container>
+      </>
   )
 }
 
